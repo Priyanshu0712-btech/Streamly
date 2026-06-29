c092bf84d5aeaa698a4d0aecc80a0deec43bd38b
Priyanshu0712-btech <priyanshu10112005@gmail.com>
2026-06-29 15:37:33 +0530

    feat: implement send friend request endpoint

--- server/src/controllers/friend.controller.js
+++ server/src/controllers/friend.controller.js
@@ -1,7 +1,27 @@
 import User from "../models/user.model.js";
 import FriendRequest from "../models/friendRequest.model.js";
+import * as friendService from "../services/friend.service.js";
 
-export const sendFriendRequest = async (req, res) => {};
+export const sendFriendRequest = async (req, res) => {
+  try {
+    const friendRequest = await friendService.sendFriendRequest(
+      req.user.id,
+      req.params.id,
+    );
+
+    res.status(201).json({
+      success: true,
+      friendRequest,
+    });
+  } catch (error) {
+    console.error("Error in sendFriendRequest:", error.message);
+
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export const acceptFriendRequest = async (req, res) => {};
 
--- server/src/services/friend.service.js
+++ server/src/services/friend.service.js
@@ -1,7 +1,62 @@
 import User from "../models/user.model.js";
 import FriendRequest from "../models/friendRequest.model.js";
 
-export const sendFriendRequest = async (senderId, recipientId) => {};
+export const sendFriendRequest = async (senderId, recipientId) => {
+  if (senderId === recipientId) {
+    throw new Error("You can't send a friend request to yourself");
+  }
+
+  const recipient = await User.findById(recipientId);
+
+  if (!recipient) {
+    throw new Error("Recipient not found");
+  }
+
+  const sender = await User.findById(senderId);
+
+  if (!sender) {
+    throw new Error("Sender not found");
+  }
+
+  if (recipient.blockedUsers.includes(senderId)) {
+    throw new Error("You cannot interact with this user");
+  }
+
+  if (sender.blockedUsers.includes(recipientId)) {
+    throw new Error("Unblock this user first");
+  }
+
+  if (recipient.friends.includes(senderId)) {
+    throw new Error("You are already friends");
+  }
+
+  const existingRequest = await FriendRequest.findOne({
+    $or: [
+      {
+        sender: senderId,
+        recipient: recipientId,
+      },
+      {
+        sender: recipientId,
+        recipient: senderId,
+      },
+    ],
+  });
+
+  if (existingRequest) {
+    throw new Error(
+      "A friend request already exists between you and this user"
+    );
+  }
+
+  const friendRequest = await FriendRequest.create({
+    sender: senderId,
+    recipient: recipientId,
+  });
+
+  return friendRequest;
+};
+
 
 export const acceptFriendRequest = async (requestId, currentUserId) => {};
 
