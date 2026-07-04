407010bb94463e5dde8549bd3fd79d4d52342354
Priyanshu0712-btech <priyanshu10112005@gmail.com>
2026-07-04 15:26:04 +0530

    feat: integrate onboarding API and redirect user after onboarding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
 
 import useAuthUser from "./hooks/useAuthUser.js";
+import Layout from "./components/Layout.jsx";
 import { useThemeStore } from "./store/useThemeStore.js";
 
 
