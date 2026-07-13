95ea1a9b45128d161707f7f04c50f5ed073b5dda
Priyanshu0712-btech <priyanshu10112005@gmail.com>
2026-07-13 17:18:27 +0530

    chore: configure production API URL

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/v1" : "/api";
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5001/api/v1",
+
+  withCredentials: true,
 });
