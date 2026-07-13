436e7c304e4b9fba4cfe353353332f58ca0e9a1d
Priyanshu0712-btech <priyanshu10112005@gmail.com>
2026-07-13 17:07:18 +0530

    fix: remove frontend static serving from backend

--- server/src/server.js
+++ server/src/server.js
@@ -2,18 +2,13 @@ import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import friendRoutes from "./routes/friend.routes.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
- 
-const __dirname = path.resolve();  
-
-
-import { connectDB } from "./config/db.js";
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -30,21 +25,18 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/friends", friendRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  // Sends the frontend's built index.html file to the client for rendering the app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+// Optional API test route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Streamly API is running",
   });
-}
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
-
